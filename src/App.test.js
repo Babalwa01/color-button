6102a9f1133c0b9ca75d4d109a4c1063a51d4c34
Babalwa Mbolekwa <babalwa.mbolekwa@clearscore.com>
2022-07-15 21:10:23 +0200

    RTL tests for button color and text

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("button has correct color before and after click", () => {
   render(<App />);
-  const linkElement = screen.getByText("Learn React");
-  expect(linkElement).toBeInTheDocument();
+  /** Find an element with a role of a button and text of 'Change to blue' */
+  const colorButton = screen.getByRole("button", { name: /change to blue/i });
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  /** click button */
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton.textContent).toBe("Change to red");
 });
