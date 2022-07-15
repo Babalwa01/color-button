bd9270b7c4199e8f6042ae4e77d996b46c15001e
Babalwa Mbolekwa <babalwa.mbolekwa@clearscore.com>
2022-07-15 23:44:07 +0200

    added test and code for checkbox label

--- src/App.test.js
+++ src/App.test.js
@@ -26,7 +26,7 @@ test("initial conditions", () => {
 test("checkbox disables button on first click and enables button on second click", () => {
   render(<App />);
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
-  const checkbox = screen.getByRole("checkbox");
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
 
   fireEvent.click(checkbox);
   expect(colorButton).toBeDisabled();
