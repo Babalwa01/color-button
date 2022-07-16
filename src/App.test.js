320a9584df9c231d71cbb996947456efc3371714
Babalwa Mbolekwa <babalwa.mbolekwa@clearscore.com>
2022-07-17 01:32:13 +0200

    configured eslint and prettier

--- src/App.test.js
+++ src/App.test.js
@@ -12,7 +12,7 @@ test("button has correct color before and after click", () => {
   /** click button */
   fireEvent.click(colorButton);
   expect(colorButton).toHaveStyle({ backgroundColor: "MidnightBlue" });
-  expect(colorButton.textContent).toBe("Change to Medium Violet Red");
+  expect(colorButton).toHaveTextContent("Change to Medium Violet Red");
 });
 
 test("initial conditions", () => {
